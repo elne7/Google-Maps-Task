717a94c2379feda3460162e54d50eb4a625cdf57
ahmedelne7 <ahmedelnahrawy881@gmail.com>
2025-03-18 14:50:11 +0200

    Add comments

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -11,24 +11,26 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  // Initialize Google Maps Controller
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
-  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{}; // Map to store markers
 
+  // Set initial camera position
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(30.033333, 31.53700030),
     zoom: 14.4746,
   );
 
-  static const CameraPosition _kLake = CameraPosition(
+  // Set Cairo location
+  static const CameraPosition _kCairo = CameraPosition(
     target: LatLng(30.033333, 31.23700030),
     zoom: 12.351926040649414,
   );
 
-
+  // Set the map controller with adding a marker
   void _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
-
     final marker = Marker(
       markerId: MarkerId('Cairo'),
       position: LatLng(30.033333, 31.23700030),
@@ -46,12 +48,14 @@ class _HomeViewState extends State<HomeView> {
         title: Text('Google Map'),
         centerTitle: true,
       ),
+      // Set the map
       body: GoogleMap(
         mapType: MapType.hybrid,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: _onMapCreated,
-        markers: markers.values.toSet(),
+        markers: markers.values.toSet(), // Set the marker
       ),
+      // Set the floating button to go to Cairo
       floatingActionButton: FloatingActionButton.extended(
         onPressed: _goToCairo,
         label: const Text('To Cairo!'),
@@ -60,8 +64,9 @@ class _HomeViewState extends State<HomeView> {
     );
   }
 
+  // Function to go to Cairo
   Future<void> _goToCairo() async {
     final GoogleMapController controller = await _controller.future;
-    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+    await controller.animateCamera(CameraUpdate.newCameraPosition(_kCairo));
   }
 }
